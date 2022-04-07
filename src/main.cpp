007a3a761b4268126f7b3dd5003879828cb4ce78
Alec McNamara <alecgmcnamara@gmail.com>
2022-04-07 11:47:16 +0100

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+ 
+const char* ssid = "SKYPEMHG";
+const char* password =  "8NHetSWQAJ75";
+const uint16_t port = 6969;
+const char * host = "192.168.0.56";  //host PC
+ 
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("...");
+  }
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+ 
+void loop()
+{
+    WiFiClient client;
+     if (!client.connect(host, port)) {
+         Serial.println("Connection to host failed");
+         delay(1000);
+        return;
+    }
+    Serial.println("Connected to server successful!");
+    client.print("Hello from ESP32!");
+    Serial.println("Disconnecting...");
+    client.stop();
+    delay(10000);
+}
---
+++ test/NetSocketTest.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket()         
+s.bind(('0.0.0.0', 6969))
+s.listen(0)                 
+while True:
+    client, addr = s.accept()
+    while True:
+        content = client.recv(32)
+        if len(content) ==0:
+           break
+        else:
+            print(content)
+    print("Closing connection")
+    client.close()
+
